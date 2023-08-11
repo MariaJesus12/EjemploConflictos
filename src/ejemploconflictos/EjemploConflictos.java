d04c46147f3f713f28a2dfa110be56424899471c
Carlos Morales <cfmorales191@gmail.com>
2023-08-11 16:08:42 -0600

    Atributos Carlos
    atributos

--- src/ejemploconflictos/EjemploConflictos.java
+++ src/ejemploconflictos/EjemploConflictos.java
@@ -15,6 +15,8 @@ public class EjemploConflictos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double edad;
+        double nombre;
     }
     
 }
